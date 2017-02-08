b8aca8e8d1513f7bfda3bdb61afd8f9bcc2b131a
Kasper Koman <kasper@Kaspers-MacBook-Pro.local>
2017-02-08 14:37:38 +0100

    Separated show- and hidefiles function in their own file

--- init.sh
+++ init.sh
@@ -5,8 +5,6 @@ export PS1="\W \u \$ "
 
 # Edit vhosts in MAMP
 alias edit_vhosts='sublime /Applications/MAMP/conf/apache/extra/httpd-vhosts.conf'
-alias showfiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-alias hidefiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 
 # Include utility scripts
 FILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/utilities"/*
---
+++ utilities/showhidefiles.sh
@@ -0,0 +1,17 @@
+# showfiles
+#
+# Show hidden files on macOS
+
+showfiles() {
+	defaults write com.apple.finder AppleShowAllFiles YES
+	killall Finder /System/Library/CoreServices/Finder.app
+}
+
+# hidefiles
+#
+# Hide again
+
+hidefiles() {
+	defaults write com.apple.finder AppleShowAllFiles NO
+	killall Finder /System/Library/CoreServices/Finder.app
+}
