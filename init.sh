65711526441b9a89019c3466c7cdf16d66cae839
Kasper Koman <kasper@Kaspers-MacBook-Pro.local>
2017-02-08 14:38:27 +0100

    Removed edit_vhosts 'cause I hope never to use MAMP again

--- init.sh
+++ init.sh
@@ -3,9 +3,6 @@
 # Cleaner terminal prefix, nomnom
 export PS1="\W \u \$ "
 
-# Edit vhosts in MAMP
-alias edit_vhosts='sublime /Applications/MAMP/conf/apache/extra/httpd-vhosts.conf'
-
 # Include utility scripts
 FILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/utilities"/*
 for f in $FILES; do 
